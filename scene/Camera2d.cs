dd888f92997bfc9d8ebb329c3194d01bc3675ad4
NijiTanso <2364728711@qq.com>
2026-08-07 09:41:46 +0800

    实现了鼠标中键拖动视角的功能

---
+++ scene/Camera2d.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Camera2d : Camera2D
+{
+    public Vector2 preciousMousePosition { get; set; }
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+        preciousMousePosition = GetGlobalMousePosition();
+
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 currentMousePosition = GetGlobalMousePosition();
+        float deltaX =  currentMousePosition.X - preciousMousePosition.X;
+        float deltaY = currentMousePosition.Y - preciousMousePosition.Y;
+
+        long clickedMouseButton = (long)(Input.GetMouseButtonMask());
+
+        if (clickedMouseButton == 4)
+        {
+            Vector2 offset = Offset;
+            offset.X -= deltaX;
+            offset.Y -= deltaY;
+            Offset = offset;
+        }
+
+        if (clickedMouseButton == 0)
+        {
+            preciousMousePosition = currentMousePosition;
+        }
+    }
+}
--- scene/MapInteraction.cs
+++ scene/MapInteraction.cs
@@ -8,6 +8,7 @@ public partial class MapInteraction : TileMapLayer
     public Vector2I PreciousClickedCell { get; set; } = new Vector2I(-999, -999);
 
     //  设置鼠标左键按下的状态和按下的时间，用于区分点击和长按
+    //  TODO: 抽象出一个 MouseInput 类，专门处理鼠标输入相关的逻辑
     public bool IsMouseLeftHolding { get; set; } = false;
     public double MouseLeftHoldingTime { get; set; }
 
@@ -33,6 +34,7 @@ public partial class MapInteraction : TileMapLayer
         //  获取当前鼠标在网格中的坐标
         Vector2 localMousePosition = GetLocalMousePosition();
         Vector2I currentCellMousePosition = LocalToMap(localMousePosition);
+        GD.Print($"当前鼠标所在的坐标：{localMousePosition}");
 
         //  获取当前鼠标点击的按钮，返回值是一个 long 类型的整数，表示鼠标按钮的掩码，1表示左键，2表示右键，4表示中键，8表示第四个按钮，16表示第五个按钮，以此类推
         long clickedMouseButton = (long)(Input.GetMouseButtonMask());
@@ -59,7 +61,7 @@ public partial class MapInteraction : TileMapLayer
 
         mouseClickTimeShower.Text = $"鼠标左键按下时间：{MouseLeftHoldingTime:F2} 秒";
 
-        GD.Print($"当前鼠标所在的坐标：{currentCellMousePosition}，当前鼠标点击的按钮掩码：{clickedMouseButton}");
+        //GD.Print($"当前鼠标所在的坐标：{currentCellMousePosition}，当前鼠标点击的按钮掩码：{clickedMouseButton}");
 
         //  获取当前鼠标所在的坐标上的瓦片 ID，如果返回 -1，说明鼠标所在的坐标上没有瓦片
         int cellId = GetCellSourceId(currentCellMousePosition);
