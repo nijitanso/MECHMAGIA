11104d12254336fc43d29527a3765db4a6f18118
NijiTanso <2364728711@qq.com>
2026-08-10 21:39:08 +0800

    想做缩放阻尼但是代码有点面目全非，废弃

--- scene/Camera2d.cs
+++ scene/Camera2d.cs
@@ -5,15 +5,24 @@ public partial class Camera2d : Camera2D
 {
     public Vector2 preciousMousePosition { get; set; }
     [Export] public float PanSpeed { get; set; } = 1.0f;
-    [Export] public float ZoomSpeed { get; set; } = 0.2f;
+    [Export] public float ZoomSpeed { get; set; } = 0.3f;
     [Export] public Vector2 MinZoom { get; set; } = new Vector2(0.6f, 0.6f);
     [Export] public Vector2 MaxZoom { get; set; } = new Vector2(4f, 4f);
+
+
+    private Vector2 targetZoom;
+    private Vector2 deltaMousePosition;
+    private Vector2 pMousePosition;
+    private bool isWheeling = false;
 
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         preciousMousePosition = GetGlobalMousePosition();
+        targetZoom = Zoom;
+        pMousePosition = GetLocalMousePosition();
+
 
     }
 
@@ -40,7 +49,20 @@ public partial class Camera2d : Camera2D
             preciousMousePosition = currentMousePosition;
         }
 
-        GD.Print($"绝对位置：{GetGlobalMousePosition()}");
+
+        
+        Zoom = Zoom.Lerp(targetZoom, 10.0f * (float)delta); // 让缩放平滑过渡到目标缩放值
+        if (AreEqualUpToTwoDecimals(Zoom.X, targetZoom.X) && isWheeling)
+        {
+            Vector2 cMousePosition = GetLocalMousePosition();
+            deltaMousePosition = cMousePosition - pMousePosition;
+            Position -= deltaMousePosition;
+            
+        }
+        GD.Print($"实际：{Zoom}");
+        GD.Print($"目标：{targetZoom}");
+        //GD.Print($"绝对位置：{GetGlobalMousePosition()}");
+        //GD.Print($"摄像机位置：{Position}");
         //GD.Print($"相对位置：{GetLocalMousePosition()}");
     }
 
@@ -56,26 +78,31 @@ public partial class Camera2d : Camera2D
             //  mouseEvent类的 ButtonIndex 属性是一个枚举类型，表示鼠标按键的索引，WheelUp 表示鼠标滚轮向上滚动，WheelDown 表示鼠标滚轮向下滚动，Pressed 属性是一个布尔类型，表示鼠标按键是否被按下
             if (mouseEvent.ButtonIndex == MouseButton.WheelUp && mouseEvent.Pressed)
             {
-
-                Vector2 pMousePosition = GetLocalMousePosition();
+                isWheeling = true;
+                pMousePosition = GetLocalMousePosition();
                 //GD.Print($"旧鼠标位置：{pMousePosition}");
 
                 //  此处并不获取滚轮的滚动量，而是用一个固定的缩放速度来调整每次鼠标滚轮时zoom的变化量（因为_Input方法是每滚一次就调用一次）
-                Zoom += new Vector2(ZoomSpeed, ZoomSpeed); // 放大
-                //FollowingMouse(); // 让摄像机跟随鼠标位置
-                Vector2 cMousePosition = GetLocalMousePosition();
-                //GD.Print($"新鼠标位置：{cMousePosition}");
-                Vector2 deltaMousePosition = cMousePosition - pMousePosition;
-                Position -= deltaMousePosition; // 让摄像机跟随鼠标位置
-                //GD.Print($"摄像机位置：{Position}");
+                //Zoom += new Vector2(ZoomSpeed, ZoomSpeed); // 放大
+                targetZoom = Zoom * (1.0f + ZoomSpeed);
+                //Position = GetGlobalMousePosition() - (pMousePosition * targetZoom);
+                
+                
+                ////FollowingMouse(); // 让摄像机跟随鼠标位置
+                //Vector2 cMousePosition = GetLocalMousePosition();
+                ////GD.Print($"新鼠标位置：{cMousePosition}");
+                //Vector2 deltaMousePosition = cMousePosition - pMousePosition;
+                //Position -= deltaMousePosition; // 让摄像机跟随鼠标位置
+                ////GD.Print($"摄像机位置：{Position}");
 
 
             }
             else if (mouseEvent.ButtonIndex == MouseButton.WheelDown && mouseEvent.Pressed)
             {
-                Zoom -= new Vector2(ZoomSpeed, ZoomSpeed); // 缩小
+                targetZoom = Zoom * (1.0f - ZoomSpeed); // 缩小
             }
         }
+        else isWheeling = false;
 
         Zoom = new Vector2(Math.Clamp(Zoom.X, MinZoom.X, MaxZoom.X), Math.Clamp(Zoom.Y, MinZoom.Y, MaxZoom.Y));     //  限制缩放范围
 
@@ -103,5 +130,12 @@ public partial class Camera2d : Camera2D
         Vector2 directionVector = new Vector2(VX, VY);
         Vector2 UnitDirectionVector = directionVector.Normalized();
         Position += directionVector * 0.25f;
+    }
+
+    public static bool AreEqualUpToTwoDecimals(float a, float b)
+    {
+        // 指定 MidpointRounding.AwayFromZero 使 .005 向上舍入，更符合日常习惯
+        return Math.Round(a, 2, MidpointRounding.AwayFromZero)
+            == Math.Round(b, 2, MidpointRounding.AwayFromZero);
     }
 }
