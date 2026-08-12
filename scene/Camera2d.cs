6f37c3f256d67569a6f615fe270eed0dbc1580c1
NijiTanso <2364728711@qq.com>
2026-08-12 22:54:19 +0800

    实现了算子与鼠标的部分交互

--- scene/Camera2d.cs
+++ scene/Camera2d.cs
@@ -9,31 +9,31 @@ public partial class Camera2d : Camera2D
     [Export] public Vector2 MaxZoom { get; set; } = new Vector2(4f, 4f);
     [Export] public float EdgePanSpeed { get; set; } = 4.0f;
 
-    private Vector2 preciousMousePosition;
-    private Vector2 newZoom;    //  这两个“new”字段用于存储缩放和平移的目标值，来实现平滑过渡效果（用Lerp方法）
-    private Vector2 newPosition;
-    private Viewport viwport;
-    private Vector2 pViewportSize;
-    private float cameraSizeX;
-    private float cameraSizeY;
+    private Vector2 _preciousMousePosition;
+    private Vector2 _newZoom;    //  这两个“new”字段用于存储缩放和平移的目标值，来实现平滑过渡效果（用Lerp方法）
+    private Vector2 _newPosition;
+    private Viewport _viwport;
+    private Vector2 _pViewportSize;
+    private float _cameraSizeX;
+    private float _cameraSizeY;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        preciousMousePosition = GetGlobalMousePosition();
-        newZoom = Zoom;
-        newPosition = Position;
+        _preciousMousePosition = GetGlobalMousePosition();
+        _newZoom = Zoom;
+        _newPosition = Position;
 
         //  获取视口的大小，用于计算摄像机的大小并实现边缘移动功能
-        viwport = GetViewport();
-        pViewportSize = viwport.GetVisibleRect().Size;
+        _viwport = GetViewport();
+        _pViewportSize = _viwport.GetVisibleRect().Size;
 
-        float viewportSizeX = pViewportSize.X;
-        float viewportSizeY = pViewportSize.Y;
-        cameraSizeX = viewportSizeX / 2.0f / Zoom.X;
-        cameraSizeY = viewportSizeY / 2.0f / Zoom.Y;
+        float viewportSizeX = _pViewportSize.X;
+        float viewportSizeY = _pViewportSize.Y;
+        _cameraSizeX = viewportSizeX / 2.0f / Zoom.X;
+        _cameraSizeY = viewportSizeY / 2.0f / Zoom.Y;
 
-        GD.Print($"摄像机大小：{cameraSizeX}, {cameraSizeY}");
+        GD.Print($"摄像机大小：{_cameraSizeX}, {_cameraSizeY}");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,25 +41,25 @@ public partial class Camera2d : Camera2D
     public override void _Process(double delta)
     {
 
-        Vector2 viewportSize = viwport.GetVisibleRect().Size;
+        Vector2 viewportSize = _viwport.GetVisibleRect().Size;
 
         //  如果视口大小发生变化或者缩放发生变化，则重新计算摄像机的大小（计算要用的理论值）
-        if (viewportSize != pViewportSize || Zoom != newZoom)
+        if (viewportSize != _pViewportSize || Zoom != _newZoom)
         {
             float viewportSizeX = viewportSize.X;
             float viewportSizeY = viewportSize.Y;
 
-            cameraSizeX = viewportSizeX / 2.0f / Zoom.X;
-            cameraSizeY = viewportSizeY / 2.0f / Zoom.Y;
+            _cameraSizeX = viewportSizeX / 2.0f / Zoom.X;
+            _cameraSizeY = viewportSizeY / 2.0f / Zoom.Y;
 
-            pViewportSize = viewportSize;
+            _pViewportSize = viewportSize;
         }
 
 
         //  这段代码实现了中建平移摄像机的功能，原理是获取鼠标当前位置和上一次鼠标位置的差值，然后根据这个差值来调整摄像机的位置，从而实现平移效果
         Vector2 currentMousePosition = GetGlobalMousePosition();
-        float deltaX = currentMousePosition.X - preciousMousePosition.X;
-        float deltaY = currentMousePosition.Y - preciousMousePosition.Y;
+        float deltaX = currentMousePosition.X - _preciousMousePosition.X;
+        float deltaY = currentMousePosition.Y - _preciousMousePosition.Y;
 
         long clickedMouseButton = (long)(Input.GetMouseButtonMask());
 
@@ -73,20 +73,20 @@ public partial class Camera2d : Camera2D
 
 
             Position = position;
-            newPosition = position;
+            _newPosition = position;
         }
 
         //  如果中建松开则更新preciousMousePosition为当前鼠标位置，这样下次按下中建时就可以正确计算鼠标的移动量
         if (clickedMouseButton == 0)
         {
-            preciousMousePosition = currentMousePosition;
+            _preciousMousePosition = currentMousePosition;
         }
 
         EdgeMove(currentMousePosition);
 
         // 让缩放和平移平滑过渡
-        Zoom = Zoom.Lerp(newZoom, 8.0f * (float)delta); 
-        Position = Position.Lerp(newPosition, 8.0f * (float)delta);
+        Zoom = Zoom.Lerp(_newZoom, 8.0f * (float)delta); 
+        Position = Position.Lerp(_newPosition, 8.0f * (float)delta);
 
         //  限制缩放范围，必须在Process方法中钳制范围是因为平滑移动
         Zoom = new Vector2(Math.Clamp(Zoom.X, MinZoom.X, MaxZoom.X), Math.Clamp(Zoom.Y, MinZoom.Y, MaxZoom.Y)); 
@@ -105,24 +105,24 @@ public partial class Camera2d : Camera2D
             //  mouseEvent类的 ButtonIndex 属性是一个枚举类型，表示鼠标按键的索引，WheelUp 表示鼠标滚轮向上滚动，WheelDown 表示鼠标滚轮向下滚动，Pressed 属性是一个布尔类型，表示鼠标按键是否被按下
             if (mouseEvent.ButtonIndex == MouseButton.WheelUp && mouseEvent.Pressed)
             {
-                Vector2 oldZoom = newZoom;
+                Vector2 oldZoom = _newZoom;
                 //  此处并不获取滚轮的滚动量，而是用一个固定的缩放速度来调整每次鼠标滚轮时zoom的变化量，做一个累乘会使每次缩放的值均匀一些（因为_Input方法是每滚一次就调用一次）
-                newZoom = Zoom * (1.0f + ZoomSpeed); // 放大
+                _newZoom = Zoom * (1.0f + ZoomSpeed); // 放大
 
                 Vector2 pMousePosition = GetLocalMousePosition();
                 //  计算缩放时鼠标位置的变化量，来调整摄像机的位置，使得缩放时鼠标位置不发生偏移
-                Vector2 cMousePosition = pMousePosition * (oldZoom / newZoom);
+                Vector2 cMousePosition = pMousePosition * (oldZoom / _newZoom);
 
                 //  对摄像机位置进行补偿
                 Vector2 deltaMousePosition = cMousePosition - pMousePosition;
-                newPosition -= deltaMousePosition;
+                _newPosition -= deltaMousePosition;
 
 
             }
             else if (mouseEvent.ButtonIndex == MouseButton.WheelDown && mouseEvent.Pressed)
             {
                 //  缩小时不用跟踪鼠标，模仿欧陆风云4的手感
-                newZoom = Zoom * (1.0f - ZoomSpeed); // 缩小
+                _newZoom = Zoom * (1.0f - ZoomSpeed); // 缩小
             }
         }
 
@@ -138,30 +138,30 @@ public partial class Camera2d : Camera2D
     private void EdgeMove(Vector2 mousePositon)
     {
         //  记住相对位置是上负下正
-        float cameraLeftEdge = Position.X - cameraSizeX;
-        float cameraRightEdge = Position.X + cameraSizeX;
-        float cameraTopEdge = Position.Y - cameraSizeY;
-        float cameraDownEdge = Position.Y + cameraSizeY;
+        float cameraLeftEdge = Position.X - _cameraSizeX;
+        float cameraRightEdge = Position.X + _cameraSizeX;
+        float cameraTopEdge = Position.Y - _cameraSizeY;
+        float cameraDownEdge = Position.Y + _cameraSizeY;
 
         //  这里直接用按键的来判断的方法可能会在以后界面多起来后有冲突，判断起来可能会很麻烦，后续也许需要重构一下
         if (mousePositon.X <= cameraLeftEdge + 5 || Input.IsKeyPressed(Key.A) || Input.IsKeyPressed(Key.Left))
         {
-            newPosition -= new Vector2(EdgePanSpeed / Zoom.X, 0);
+            _newPosition -= new Vector2(EdgePanSpeed / Zoom.X, 0);
         }
 
         if (mousePositon.X >= cameraRightEdge - 5 || Input.IsKeyPressed(Key.D) || Input.IsKeyPressed(Key.Right))
         {
-            newPosition += new Vector2(EdgePanSpeed / Zoom.X, 0);
+            _newPosition += new Vector2(EdgePanSpeed / Zoom.X, 0);
         }
 
         if (mousePositon.Y <= cameraTopEdge + 5 || Input.IsKeyPressed(Key.W) || Input.IsKeyPressed(Key.Up))
         {
-            newPosition -= new Vector2(0, EdgePanSpeed / Zoom.X);
+            _newPosition -= new Vector2(0, EdgePanSpeed / Zoom.X);
         }
 
         if (mousePositon.Y >= cameraDownEdge - 5 || Input.IsKeyPressed(Key.S) || Input.IsKeyPressed(Key.Down))
         {
-            newPosition += new Vector2(0, EdgePanSpeed / Zoom.X);
+            _newPosition += new Vector2(0, EdgePanSpeed / Zoom.X);
         }
 
     }
---
+++ scene/Counter.cs
@@ -0,0 +1,116 @@
+using Godot;
+using System;
+
+public partial class Counter : Area2D
+{
+	//	这些数值将从数值类中获取
+	public int AttackPoint { get; set; }
+	public int DefendkPoint { get; set; }
+	public int MovePoint { get; set; }
+
+	private Label _attackPointLabel;
+	private Label _defendPointLabel;
+	private Label _movePointLabel;
+	private CollisionShape2D _collisionShape2D;
+	private Node2D _upperLayer;
+
+	private Vector2 _topLeftPosition;
+	private Vector2 _topRightPosition;
+	private Vector2 _downLeftPosition;
+	private Vector2 _downRightPosition;
+
+	private Vector2 _size;
+
+	private bool _isHovering = false;
+	private bool _isSelected = false;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_attackPointLabel = GetNode<Label>("AttackPointLabel");
+		_defendPointLabel = GetNode<Label>("DefendPointLabel");
+		_movePointLabel = GetNode<Label>("MovePointLabel");
+		_collisionShape2D = GetNode<CollisionShape2D>("CollisionShape2D");
+		_upperLayer = GetNode<Node2D>("UpperLayer");
+
+		_size = _collisionShape2D.Shape.GetRect().Size;
+		_topLeftPosition = new Vector2(-_size.X / 2.0f, -_size.Y / 2.0f);
+		_topRightPosition = new Vector2(_size.X / 2.0f, -_size.Y / 2.0f);
+		_downLeftPosition = new Vector2(-_size.X / 2.0f, _size.Y / 2.0f);
+		_downRightPosition = new Vector2(_size.X / 2.0f, _size.Y / 2.0f);
+
+		GD.Print(_topLeftPosition);
+
+		MouseEntered += HoveringHighlight;
+		MouseExited += NotHoveringHighlight;
+
+		QueueRedraw();
+
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//GD.Print(GetGlobalMousePosition());
+	}
+
+	public void HoveringHighlight()
+	{
+		_isHovering = true;
+		Scale = new Vector2(1.07f, 1.07f);
+		QueueRedraw();
+	}
+	public void NotHoveringHighlight()
+	{
+		_isHovering = false;
+		Scale = new Vector2(1.0f, 1.0f);
+		QueueRedraw();
+	}
+
+	public override void _Draw()
+	{
+
+
+		if (_isHovering && !_isSelected)
+		{
+			DrawDashedLine(_topLeftPosition, _topRightPosition, Color.Color8(255, 255, 255), width: 4.0f);
+			DrawDashedLine(_topLeftPosition, _downLeftPosition, Color.Color8(255, 255, 255), width: 4.0f);
+			DrawDashedLine(_downLeftPosition, _downRightPosition, Color.Color8(255, 255, 255), width: 4.0f);
+			DrawDashedLine(_downRightPosition, _topRightPosition, Color.Color8(255, 255, 255), width: 4.0f);
+
+		}
+
+		if (_isSelected)
+		{
+			DrawDashedLine(_topLeftPosition, _topRightPosition, Color.Color8(255, 255, 0), width: 5.0f);
+			DrawDashedLine(_topLeftPosition, _downLeftPosition, Color.Color8(255, 255, 0), width: 5.0f);
+			DrawDashedLine(_downLeftPosition, _downRightPosition, Color.Color8(255, 255, 0), width: 5.0f);
+			DrawDashedLine(_downRightPosition, _topRightPosition, Color.Color8(255, 255, 0), width: 5.0f);
+		}
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed && _isHovering)
+			{
+				if (!_isSelected)
+				{
+                    _isSelected = true;
+                    QueueRedraw();
+                }
+				else
+				{
+                    _isSelected = false;
+                    QueueRedraw();
+                }
+				
+
+
+				
+			}
+		}
+	}
+}
